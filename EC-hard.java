64879eb69edef240e0b1949b360abb7cd735170f
Mell777 <mttnglymell@gmail.com>
2016-10-28 16:38:19 -0400

    Update EC-hard.java

--- EC-hard.java
+++ EC-hard.java
@@ -1,7 +1,32 @@
 package whateverthifuckyouwant;
 public class Whateverthefuckyouwant {
-    public static void main(String[] args) {
+    int piece[][] = new int[3][3];
+        int board[][]= new int[6][3];
+        System.out.println("give me what your piece looks like mate!");
+        populateArray(piece);
+        System.out.println("give me what board we're working with please!");
+        populateArray(board);
         
+
     }
-    
+
+    static void populateArray(int array[][]) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                array[i][j] = 3;
+                while (array[i][j] == 3) {
+                    switch (input.next()) {
+                        case "_":
+                            array[i][j] = 0;
+                            break;
+                        case "x":
+                            array[i][j] = 1;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+        }
+    }
 }
