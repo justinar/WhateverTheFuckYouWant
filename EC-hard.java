3d0939e4c3b6b4cecf7ec4258f5a2bcb76182fac
sujiko <qureshi.ak225@gmail.com>
2016-10-29 18:32:36 -0400

    finally poulated arrays correctly
    this should now populate the 2-d arrays with 1s or 0s  now what do i do with this.

--- EC-hard.java
+++ EC-hard.java
@@ -1,45 +1,88 @@
 package whateverthifuckyouwant;
 public class Whateverthefuckyouwant {
-    Scanner s= new Scanner;
-        int piece[][] = {{0,0,0},
-                         {0,0,0},
-                         {0,0,0}};
-        int board[][]= {{0,0,0,0,0,0},
-                        {0,0,0,0,0,0},
-                        {0,0,0,0,0,0};
-        System.out.println("give me what your piece looks like mate!");
-        populateArray(piece);
-        System.out.println("give me what board we're working with please!");
-        populateArray(board);
-    
-    
-    
-        
+Scanner input = new Scanner(System.in);
+        String arrayToHoldInputs[] = new String[3];
+        int peice[][] = new int[3][3];
+        int board[][] = new int[6][3];
+
+        System.out.println("Show me what the peice looks like :3");
+        arrayToHoldInputs[0] = input.next();
+        arrayToHoldInputs[1] = input.next();
+        arrayToHoldInputs[2] = input.next();
+        populateArray(peice, arrayToHoldInputs);
+        System.out.println("wanna show me what the board looks like??");
+        arrayToHoldInputs[0] = input.next();
+        arrayToHoldInputs[1] = input.next();
+        arrayToHoldInputs[2] = input.next();
+        populateArray(board, arrayToHoldInputs);
 
     }
 
-    static void populateArray(int array[][]) {
-        Scanner s = new Scanner;
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[i].length; j++) {
-                char test = input.nextChar();
-                /*
-                array[i][j] = 3;
-                while (array[i][j] == 3) {
-                    switch (input.next()) {
-                        case "_":
-                            array[i][j] = 0;
-                            break;
-                        case "x":
-                            array[i][j] = 1;
-                            break;
-                        default:
-                            break;
+    static void populateArray(int array[][], String arrayHoldingInputs[]) {
+        for (int i = 0; i < array[0].length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                if (arrayHoldingInputs[i].length() == 3) {
+                    if (j == 0) {
+                        if (arrayHoldingInputs[i].charAt(j) == 'x') {
+                            array[j][i] = 1;
+                        } else if (arrayHoldingInputs[i].charAt(j) == '_') {
+                            array[j][i] = 0;
+                        }
+                    } else if (j == 1) {
+                        if (arrayHoldingInputs[i].charAt(j) == 'x') {
+                            array[j][i] = 1;
+                        } else if (arrayHoldingInputs[i].charAt(j) == '_') {
+                            array[j][i] = 0;
+                        }
+                    } else if (j == 2) {
+                        if (arrayHoldingInputs[i].charAt(j) == 'x') {
+                            array[j][i] = 1;
+                        } else if (arrayHoldingInputs[i].charAt(j) == '_') {
+                            array[j][i] = 0;
+                        }
                     }
-                    */
+                }else if (arrayHoldingInputs[i].length() == 3){
+                    if (j == 0) {
+                        if (arrayHoldingInputs[i].charAt(j) == 'x') {
+                            array[j][i] = 1;
+                        } else if (arrayHoldingInputs[i].charAt(j) == '_') {
+                            array[j][i] = 0;
+                        }
+                    } else if (j == 1) {
+                        if (arrayHoldingInputs[i].charAt(j) == 'x') {
+                            array[j][i] = 1;
+                        } else if (arrayHoldingInputs[i].charAt(j) == '_') {
+                            array[j][i] = 0;
+                        }
+                    } else if (j == 2) {
+                        if (arrayHoldingInputs[i].charAt(j) == 'x') {
+                            array[j][i] = 1;
+                        } else if (arrayHoldingInputs[i].charAt(j) == '_') {
+                            array[j][i] = 0;
+                        }
+                    }else if (j == 3) {
+                        if (arrayHoldingInputs[i].charAt(j) == 'x') {
+                            array[j][i] = 1;
+                        } else if (arrayHoldingInputs[i].charAt(j) == '_') {
+                            array[j][i] = 0;
+                        }
+                    } else if (j == 4) {
+                        if (arrayHoldingInputs[i].charAt(j) == 'x') {
+                            array[j][i] = 1;
+                        } else if (arrayHoldingInputs[i].charAt(j) == '_') {
+                            array[j][i] = 0;
+                        }
+                    } else if (j == 5) {
+                        if (arrayHoldingInputs[i].charAt(j) == 'x') {
+                            array[j][i] = 1;
+                        } else if (arrayHoldingInputs[i].charAt(j) == '_') {
+                            array[j][i] = 0;
+                        }
+                    }
                 }
             }
+
         }
-        return;
+
     }
 }
