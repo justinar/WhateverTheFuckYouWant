a5ebf7cb1acde810f1dee28fa655f9791b0f5c80
sujiko <qureshi.ak225@gmail.com>
2016-10-30 19:54:04 -0400

    shorten
    shortened the code to pupulate the array. also makes sure the array is bottom aligned

--- EC-hard.java
+++ EC-hard.java
@@ -1,6 +1,8 @@
 package whateverthifuckyouwant;
+import java.util.Scanner
 public class Whateverthefuckyouwant {
-Scanner input = new Scanner(System.in);
+
+        Scanner input = new Scanner(System.in);
         String arrayToHoldInputs[] = new String[3];
         int piece[][] = new int[3][3];
         int board[][] = new int[6][3];
@@ -29,78 +31,27 @@ Scanner input = new Scanner(System.in);
                 }else if (j==0){
                     piece[i][j]=0;
                 }
-                    System.out.print(piece[i][j]);
             }
         }
         }
+        
     }
+        
     }
 
     static void populateArray(int array[][], String arrayHoldingInputs[]) {
         for (int i = 0; i < array[0].length; i++) {
             for (int j = 0; j < array.length; j++) {
-                if (arrayHoldingInputs[i].length() == 3) {
-                    if (j == 0) {
                         if (arrayHoldingInputs[i].charAt(j) == 'x') {
                             array[j][i] = 1;
                         } else if (arrayHoldingInputs[i].charAt(j) == '_') {
                             array[j][i] = 0;
                         }
-                    } else if (j == 1) {
-                        if (arrayHoldingInputs[i].charAt(j) == 'x') {
-                            array[j][i] = 1;
-                        } else if (arrayHoldingInputs[i].charAt(j) == '_') {
-                            array[j][i] = 0;
-                        }
-                    } else if (j == 2) {
-                        if (arrayHoldingInputs[i].charAt(j) == 'x') {
-                            array[j][i] = 1;
-                        } else if (arrayHoldingInputs[i].charAt(j) == '_') {
-                            array[j][i] = 0;
-                        }
-                    }
-                }else if (arrayHoldingInputs[i].length() == 6){
-                    if (j == 0) {
-                        if (arrayHoldingInputs[i].charAt(j) == 'x') {
-                            array[j][i] = 1;
-                        } else if (arrayHoldingInputs[i].charAt(j) == '_') {
-                            array[j][i] = 0;
-                        }
-                    } else if (j == 1) {
-                        if (arrayHoldingInputs[i].charAt(j) == 'x') {
-                            array[j][i] = 1;
-                        } else if (arrayHoldingInputs[i].charAt(j) == '_') {
-                            array[j][i] = 0;
-                        }
-                    } else if (j == 2) {
-                        if (arrayHoldingInputs[i].charAt(j) == 'x') {
-                            array[j][i] = 1;
-                        } else if (arrayHoldingInputs[i].charAt(j) == '_') {
-                            array[j][i] = 0;
-                        }
-                    }else if (j == 3) {
-                        if (arrayHoldingInputs[i].charAt(j) == 'x') {
-                            array[j][i] = 1;
-                        } else if (arrayHoldingInputs[i].charAt(j) == '_') {
-                            array[j][i] = 0;
-                        }
-                    } else if (j == 4) {
-                        if (arrayHoldingInputs[i].charAt(j) == 'x') {
-                            array[j][i] = 1;
-                        } else if (arrayHoldingInputs[i].charAt(j) == '_') {
-                            array[j][i] = 0;
-                        }
-                    } else if (j == 5) {
-                        if (arrayHoldingInputs[i].charAt(j) == 'x') {
-                            array[j][i] = 1;
-                        } else if (arrayHoldingInputs[i].charAt(j) == '_') {
-                            array[j][i] = 0;
-                        }
+                    
                     }
                 }
             }
 
         }
 
-    }
-}
+    
