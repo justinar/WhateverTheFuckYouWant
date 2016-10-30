27cb3065142731afb37bfbb60d0ead14b35b6f3f
sujiko <qureshi.ak225@gmail.com>
2016-10-29 20:14:00 -0400

    bottom alignment of piece
    this make sure the piece is bottom alligned in the 2d array for piece

--- EC-hard.java
+++ EC-hard.java
@@ -2,20 +2,38 @@ package whateverthifuckyouwant;
 public class Whateverthefuckyouwant {
 Scanner input = new Scanner(System.in);
         String arrayToHoldInputs[] = new String[3];
-        int peice[][] = new int[3][3];
+        int piece[][] = new int[3][3];
         int board[][] = new int[6][3];
 
         System.out.println("Show me what the peice looks like :3");
         arrayToHoldInputs[0] = input.next();
         arrayToHoldInputs[1] = input.next();
         arrayToHoldInputs[2] = input.next();
-        populateArray(peice, arrayToHoldInputs);
+        populateArray(piece, arrayToHoldInputs);
         System.out.println("wanna show me what the board looks like??");
         arrayToHoldInputs[0] = input.next();
         arrayToHoldInputs[1] = input.next();
         arrayToHoldInputs[2] = input.next();
         populateArray(board, arrayToHoldInputs);
-
+        int doesPieceTouchBottom=0;
+        
+        for (int j=0; j< piece.length; j++){
+            if (piece[j][2] == 1){
+                doesPieceTouchBottom= 1;    
+        }
+        if (doesPieceTouchBottom == 0){
+            for (j=piece[0].length-1; j>-1;j--){
+                for(int i=0;i<piece.length; i++){
+                    if (j>0){
+                    piece[i][j]=piece[i][j-1];
+                }else if (j==0){
+                    piece[i][j]=0;
+                }
+                    System.out.print(piece[i][j]);
+            }
+        }
+        }
+    }
     }
 
     static void populateArray(int array[][], String arrayHoldingInputs[]) {
